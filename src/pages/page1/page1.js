b62aa0da58a3e735afdbdc30fc5c700704b468c4
Maitrey Bhute <maitreybhute2806@gmail.com>
2022-06-24 16:55:18 +0530

    firstCommit

--- src/pages/page1/page1.js
+++ src/pages/page1/page1.js
@@ -5,6 +5,7 @@ const Page1 = () => {
     return(
         <div id="page-1">
             Here is the page 1
+            This is the first page to be opened
             <a href="/page2">page-2</a>
         </div>
     );
