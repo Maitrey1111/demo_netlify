4ce9f85bf0f1dc46f0b6a96944746661647ecd44
Maitrey Bhute <maitreybhute2806@gmail.com>
2022-06-24 16:47:18 +0530

    firstCommit

---
+++ src/pages/page1/page1.js
@@ -0,0 +1,14 @@
+import "./page1.css"
+
+const Page1 = () => {
+
+    return(
+        <div id="page-1">
+            Here is the page 1
+            <a href="/page2">page-2</a>
+        </div>
+    );
+
+}
+
+export default Page1;
---
+++ src/pages/page2/page2.js
@@ -0,0 +1,14 @@
+import "./page2.css"
+
+const Page2 = () => {
+
+    return(
+        <div id="page-2">
+            Here is the page 2
+            <a href="/">page-1</a>
+        </div>
+    );
+
+}
+
+export default Page2;
