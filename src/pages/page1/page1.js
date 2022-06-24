67cbb32c2f67a7a9c74a8ce9ea2254fd74096dbf
Maitrey Bhute <maitreybhute2806@gmail.com>
2022-06-24 17:22:32 +0530

    firstCommit

--- src/pages/page1/page1.js
+++ src/pages/page1/page1.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./page1.css"
 
 const Page1 = () => {
+
+    const [Var, setVar] = useState(second)
 
     return(
         <div id="page-1">
             Here is the page 1
             This is the first page to be opened
-            <a href="/page2">page-2</a>
+            <a href="/page2" onClick={setVar(1)}>page-2</a>
         </div>
     );
 
