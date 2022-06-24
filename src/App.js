b72927f3b3cf1a9cd30797897173baed74f13930
Maitrey Bhute <maitreybhute2806@gmail.com>
2022-06-24 17:18:41 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Page1 from './pages/page1/page1.js';
 import Page2 from './pages/page2/page2.js';
 
@@ -7,12 +7,12 @@ import './App.css';
 function App() {
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Page1 />}></Route>
           <Route path="/page2" element={<Page2 />}></Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
